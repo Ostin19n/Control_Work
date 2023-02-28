f1ba76921f58ac234e65faba925b2084542cccee
Anastasya <anastasiaostin@mail.ru>
2023-02-28 12:49:28 +0300

    Редактирование вывода данных

--- Code/Program.cs
+++ Code/Program.cs
@@ -23,20 +23,20 @@ for (int i = 0; i < size; i++)
 }
 string[] arrayElements = new string[size];
 int length = 3;
-int position = 0;
+int numberelements = 0;
 
 for (int x = 0; x < size; x++)
 {
     if (arrayLine[x].Length <= length)
     {
-        arrayElements[position] = arrayLine[x];
-        position++;
+        arrayElements[numberelements] = arrayLine[x];
+        numberelements++;
     }
 }
 Console.WriteLine();
-PrintArray(arrayElements);
+ShowArray(arrayElements);
 
-void PrintArray(string[] array)
+void ShowArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {

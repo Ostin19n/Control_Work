7db687a789ba72b3edc9f0921c8091f8c6048849
Anastasya <anastasiaostin@mail.ru>
2023-02-28 12:44:17 +0300

    Создание первоначального массива

--- Code/Program.cs
+++ Code/Program.cs
@@ -12,3 +12,12 @@ int GetNum(string text)
 }
     int numberN = GetNum("Введите количество элементов: ");
 
+
+int size = numberN;
+string[] arrayLine = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine())!;
+    arrayLine[i] = element;
+}
